24e26bf0de90b3188f79f9b3d9c6391d9aa2a8b6
Yash Srivastav <yash111998@gmail.com>
2019-04-14 22:07:41 +0530

    :bug: fix bug with config variables initialization

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,27 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"strings"
 
 	"github.com/spf13/viper"
 
+	_ "blockchain-voting/config"
 	"blockchain-voting/http"
 )
 
 func main() {
-	// read from config file
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Println("[warn] unable to locate config file")
-	}
-
-	// Auto read config values from env
-	viper.SetEnvPrefix("voting")
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
 
 	host := viper.GetString("http.host")
 	port := viper.GetString("http.port")
